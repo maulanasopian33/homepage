d1ad989f18ca3c48ca406c871d6e7ed580c9c881
maulanasopian <maulanasopian12@gmail.com>
2022-11-20 12:41:40 +0700

    add tailwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,6 +27,7 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <button class="bg-gray-500 m-4 w-full rounded-md">aa</button>
   </div>
 </template>
 
